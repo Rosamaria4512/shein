c00314ed776ce8f590d89e30435bed3731864274
lenovo.esucre@gmail.com <felipe3-A>
2023-09-18 22:01:27 -0500

    No funcionan

---
+++ src/main/java/com/co/shein/questions/ValidarContenedor.java
@@ -0,0 +1,13 @@
+package com.co.shein.questions;
+
+import com.co.shein.userinterfaces.LaptopInterface;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+import org.openqa.selenium.WebElement;
+
+public class ValidarContenedor implements Question {
+    @Override
+    public Object answeredBy(Actor actor) {
+        return null;
+    }
+}
